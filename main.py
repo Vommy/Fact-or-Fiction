0903b7d6dac23506f3a48b4291101ca25a5ed3f3
Veren Villegas <verenvillegas@gmail.com>
2024-03-11 22:47:46 +1300

    FEATURE: Context conversations logic added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.classes import Game
 import src.utilities
+import requests
+import json
 
 game_state = Game.Game()
 
@@ -18,4 +20,37 @@ src.utilities.print_menu()
     #Show the user their high score
     #Allow them to restart the game
 
+context = []
 
+
+content = '{"role": "user", "content": "Generate a fact that is true or false so I can guess whether it is fact or fiction"}'
+
+content = json.loads(content)
+
+context.append(content)
+
+url = f"http://localhost:3000/chatgpt-req?content={context}"
+
+response = requests.get(url)
+
+response_json = response.json()
+
+context.append(response_json)
+print(response_json)
+
+print(f"Context: {context}")
+
+ans = input("Type true or false:")
+
+content = '{"role": "user", "content": "%s"}' % ans
+content = json.loads(content)
+
+context.append(content)
+
+
+url = f"http://localhost:3000/chatgpt-req?content={context}"
+
+response = requests.get(url)
+
+response_json = response.json()
+print(response_json)
