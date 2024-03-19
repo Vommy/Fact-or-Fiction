d41776b183253f2c0271f57a84e78793dc148a40
Veren Villegas <verenvillegas@gmail.com>
2024-03-19 17:41:25 +1300

    updated comments

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ src.utilities.print_menu(data["menu_options"])
 while(True):
     values = src.utilities.menu()
     if(values):
+            #If the game is not over
+        #Give them a question
+        #Let them answer
+        #Print whether they got it right or wrong
+        #Adjust the game accordingly
+        #Show the user their high score
+        #Allow them to restart the game
         game_state.add_question(values["question"])
         src.utilities.print_menu(data["sub_menu_options"])
     else:
@@ -26,12 +33,6 @@ while(True):
         time.sleep(5)
         src.utilities.clear_std_out()
         src.utilities.print_menu(data["menu_options"])
-    #If the game is not over
-        #Give them a question
-        #Let them answer
-        #Print whether they got it right or wrong
-        #Adjust the game accordingly
-    #Show the user their high score
-    #Allow them to restart the game
+
 
 #This game will require keeping track of context.
